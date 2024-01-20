ef655382e2993b7f694aef7586fa0301508240c7
Kaardal88 <123365145+Kaardal88@users.noreply.github.com>
2024-01-20 15:06:31 +0100

    This is all of my commits at once

---
+++ js/fetch.js
@@ -0,0 +1,68 @@
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const apiUrl = 'https://api.noroff.dev/api/v1/gamehub';
+    const gameCatalogueElement = document.getElementById('game-catalogue');
+    const loadingIndicator = document.getElementById('loadingIndicator');
+    const loadingSpinner = document.querySelector(".loadingSpinner");
+  
+ 
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to fetch games: ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            data.forEach(game => {
+                const gameElement = createGameElement(game, loadingIndicator);
+                gameCatalogueElement.appendChild(gameElement); 
+                const displayedGames = [];
+                let length = data.length = 4;       
+            });
+
+            
+          
+        
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+});
+
+
+ 
+function createGameElement(game, loadingIndicator) {
+    const gameElement = document.createElement('div');
+    gameElement.innerHTML = `
+        <div class="game-cover-background">
+            <div class="game-cover" style="background-image: url('${game.image}');"></div>
+            <div class="spaced-row">
+                <div class="game-text">
+                    <p class="game-text-big">${game.title}</p>
+                </div>
+            </div>
+        </div>
+    `;
+
+    
+ 
+   
+    // Add click event listener
+    gameElement.addEventListener('click', () => {
+        // Show loading indicator
+        loadingIndicator.style.display = 'block';
+        
+ 
+        // Wait for 1 seconds before redirecting
+        setTimeout(() => {
+            window.location.href = `details.html?id=${game.id}`;
+        }, 1000); 
+    });
+ 
+    return gameElement;
+}
---
+++ js/fetch01.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const gameId = urlParams.get('id');
+    
+ 
+    if (gameId) {
+        const apiUrl = `https://api.noroff.dev/api/v1/gamehub/${gameId}`;
+        fetch(apiUrl)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Failed to fetch game details: ' + response.statusText);
+                }
+                return response.json();
+            })
+            .then(gameDetails => {
+                displayGameDetails(gameDetails);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    } else {
+        // Handle case where no game ID is provided in URL
+        console.error('Game ID not provided.');
+    }
+});
+ 
+function displayGameDetails(gameDetails) {
+    const detailsContainer = document.getElementById('game-details');
+    detailsContainer.innerHTML = `
+<img class="displayedImg" src="${gameDetails.image}" alt="${gameDetails.title}">
+<div class="background-info">
+<h1 id="displayedGameTitle">${gameDetails.title}</h1>
+<p class="description-textbox"><b>About the Game:</b> ${gameDetails.description}</p>
+<p><b>Genre:</b> ${gameDetails.genre}</p>
+<p><b>Released:</b> ${gameDetails.released}</p>
+<p><b>Age Limit:</b> ${gameDetails.ageRating}</p>
+<h3><b>Price:</b> ${gameDetails.price}</h3>
+</div>
+
+        
+    `;
+
+}
